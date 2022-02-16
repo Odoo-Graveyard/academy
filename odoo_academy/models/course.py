2ab7c9850d1febf7f9f314fd1cae1bf349f52ec2
Hinamizawa <themoonbunnys@gmail.com>
2022-02-16 16:38:23 -0400

    Update course.py

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -6,3 +6,14 @@ from odoo import models, fields, api
 class Course (models.Model):
     _name = 'academy.course'
     _description = 'Course Info'
+
+
+name = fields.Char(sting='Title', required=True)
+description = fields.Text(string='Description')
+level = fields.Selection(string='Level',
+                         selection=[('beginner', 'Beginner'),
+                                    ('intermediate', 'Intermediate'),
+                                    ('advanced', 'Advanced')],
+                         copy=False)
+
+active = fields.Boolean(string='active', default=True)
