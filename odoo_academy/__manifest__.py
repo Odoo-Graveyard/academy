285ccd046e6602ce8d6677b5a44398467ddd245e
Hinamizawa <themoonbunnys@gmail.com>
2022-02-16 15:42:53 -0400

    s

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    'name': 'Odoo Academy',
+
+    'summary': """App to do simple accounting """,
+
+    'description': """
+        Training Module
+        - level
+        - Training
+        - Interactive
+    
+    """,
+    'author': 'Hinamizawa',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+
+
+    ],
+    'demo': [
+
+
+    ],
+
+
+
+
+}
