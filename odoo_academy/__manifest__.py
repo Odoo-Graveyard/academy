9b1848803702fe7d7b7e74c00952bd2235e2955c
Hinamizawa <themoonbunnys@gmail.com>
2022-02-21 10:07:30 -0400

    Update __manifest__.py

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -23,8 +23,8 @@
     'depends': ['base'],
 
     'data': [
-        # 'security/academy_security.xml',
-        # 'security/ir.model.access.csv',
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
 
     ],
     'demo': [
